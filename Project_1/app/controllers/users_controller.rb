2f91de7d6d7595c4cada81f138b6907e70fde3d3
Dylan-Barclay <dr.barclay99@gmail.com>
2021-08-10 22:09:45 -0500

    Updateing

--- Project_1/app/controllers/users_controller.rb
+++ Project_1/app/controllers/users_controller.rb
@@ -15,6 +15,17 @@ class UsersController < ApplicationController
         redirect_to '/say/hello/joe'
     end
 
-
+    def counter
+        if session[:counter]
+            session[:counter] += 1
+        else
+            session[:counter] = 1
+        end
+    end
+    
+    def restart
+        session[:counter] = 0
+        redirect_to "/times"
+    end
 
 end
--- Project_1/config/routes.rb
+++ Project_1/config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
 
   root 'users#index'
   get '' => 'users#index'
+
+  get "times" => "users#counter"
+  get "times/restart" => "users#restart"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
