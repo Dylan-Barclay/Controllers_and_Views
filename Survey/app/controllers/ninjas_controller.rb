108faf4cd1a0549b4a46b84cb32ebb657c7c473f
Dylan-Barclay <dr.barclay99@gmail.com>
2021-08-11 00:29:09 -0500

    survey

---
+++ Survey/app/controllers/ninjas_controller.rb
@@ -0,0 +1,20 @@
+class NinjasController < ApplicationController
+    @@counter = 0
+    def index
+    end
+    def create
+        @new_ninja = Ninja.create(ninja_params)
+        redirect_to '/results'
+    end
+    
+    def results
+        @@counter += 1
+        flash[:notice] = "You are Ninja #{@@counter}"
+        @show_ninja = Ninja.last
+    end
+
+    private
+        def ninja_params
+        params.require(:ninja).permit(:name, :location, :language, :comment)
+    end
+end
