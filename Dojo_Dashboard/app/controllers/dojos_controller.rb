8177535bfc2ac314a250fb19bc1584962002e7df
Dylan-Barclay <dr.barclay99@gmail.com>
2021-08-13 18:26:14 -0500

    Updateing to Dojo Dashboard

--- Dojo_Dashboard/app/controllers/dojos_controller.rb
+++ Dojo_Dashboard/app/controllers/dojos_controller.rb
@@ -4,16 +4,40 @@ class DojosController < ApplicationController
     end
 
     def new 
-
     end
-    def create
 
+    def create
         Dojo.create(branch: params[:dojo][:branch], street: params[:dojo][:street], city: params[:dojo][:city], state: params[:dojo][:state])
+        
         redirect_to ''
     end
-    # private 
-    #     def
-    #         params.require(:dojo).permit(:branch,:street,:city,:state)
-    #     end
+    def edit
+        @dojo = Dojo.find(params[:id])
+    end
+
+
+    def show
+        @dojo = Dojo.find(params[:id])
+
+    end
+
+    def update
+        dojo = Dojo.find(params[:id])
+        dojo.update(dojo_params)
+        dojo.save
+        puts dojo.errors.full_messages if dojo.errors
+        redirect_to ''
+    end
+
+    def destroy
+        dojo = Dojo.find(params[:id])
+        dojo.destroy
+        redirect_to ''
+    end
+
+    private 
+        def dojo_params
+            params.require(:dojo).permit(:branch, :street, :city, :state)
+        end
 
 end
